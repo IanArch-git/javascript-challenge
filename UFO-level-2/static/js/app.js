c7bb9ffe2700b9ee89abb517c86c25af24c72906
Ian Arch <ian.arch01@gmail.com>
2021-04-27 09:20:11 -0600

    Level 2 set up

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -1,4 +1,35 @@
-// from data.js
 var tableData = data;
+// console.log(tableData);
+
+var tbody = d3.select("tbody");
+tableData.forEach(function(ufo){
+    // console.log(ufo);
+    var row = tbody.append("tr");
+    Object.entries(ufo).forEach(function([key,value]){
+        // console.log(key,value);
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
+
+var button = d3.select("#filter-btn");
+button.on("click",function(event){
+    d3.event.preventDefault();
+    tbody.html("");
+    var filterDate = d3.select("#datetime");
+    var filterValue = filterDate.property("value");
+    // console.log(filterValue);
+    var filterData = tableData.filter(tableData => tableData.datetime === filterValue);
+    // console.log(filterData);
+    filterData.forEach(function(ufoDate){
+        // console.log(ufoDate);
+        var row = tbody.append("tr");
+        Object.entries(ufoDate).forEach(function([key,value]){
+            // console.log(key,value);
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+});
 
 
