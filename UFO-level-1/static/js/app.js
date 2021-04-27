4101c56b7be3b47008d673df8f639eff74c3b62e
Ian Arch <ian.arch01@gmail.com>
2021-04-27 08:51:42 -0600

    initial button setup

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,13 +1,24 @@
 var tableData = data;
 // console.log(tableData);
 
-var tbody = d3.select("tbody")
+var tbody = d3.select("tbody");
 tableData.forEach(function(ufo){
-    console.log(ufo);
+    // console.log(ufo);
     var row = tbody.append("tr");
     Object.entries(ufo).forEach(function([key,value]){
-        console.log(key,value);
+        // console.log(key,value);
         var cell = row.append("td");
         cell.text(value);
     });
+});
+
+var button = d3.select("#filter-btn");
+button.on("click",function(event){
+    d3.event.preventDefault();
+    tbody.html("");
+    var filterDate = d3.select("#datetime");
+    var filterValue = filterDate.property("value");
+    console.log(filterValue);
+    var filterData = tableData.filter(tableData => tableData.datetime === filterValue);
+    console.log(filterData);
 });
